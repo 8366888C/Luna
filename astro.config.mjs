b2fa32bafb97dc8674ca21204d811080a29df929
Rahul <deploy.rahul@gmail.com>
2026-04-08 03:19:48 +0530

    feat: add introduction section, navigation, and theme toggle

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), sitemap(), mdx()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: "hover",
+  },
+
+  site: "https://ryze.pages.dev",
+});
