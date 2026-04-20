185359e58b568ef051177a68fc4538b3fa28c34d
Rahul <deploy.rahul@gmail.com>
2026-04-21 02:59:44 +0530

    feat: add rss

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,18 +1,39 @@
 import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import { parse } from "date-fns";
+import sanitizeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
+const parser = new MarkdownIt();
 
-export function GET(context: { site: any }) {
+export async function GET(context: { site: any }) {
+  const posts = (await getCollection("blog"))
+    .filter((post) => !post.data.draft)
+    .sort((a, b) => {
+      const aDate = parse(a.data.date, "dd-MM-yyyy", new Date());
+      const bDate = parse(b.data.date, "dd-MM-yyyy", new Date());
+      return bDate.getTime() - aDate.getTime();
+    });
+
   return rss({
-    // `<title>` field in output xml
-    title: "Buzz’s Blog",
-    // `<description>` field in output xml
-    description: "A humble Astronaut’s guide to the stars",
-    // Pull in your project "site" from the endpoint context
-    // https://docs.astro.build/en/reference/api-reference/#site
+    trailingSlash: false,
+    title: "Ryze Blog",
+    description:
+      "A reader-friendly blog with accessibility, SEO and responsiveness out of the box",
     site: context.site,
-    // Array of `<item>`s in output xml
-    // See "Generating items" section for examples using content collections and glob imports
-    items: [],
-    // (optional) inject custom xml
+    items: posts.map((post) => ({
+      link: `/blog/${post.id}`,
+      pubDate: parse(post.data.date, "dd-MM-yyyy", new Date()),
+      title: post.data.title,
+      description: post.data.description,
+      categories: [post.data.category, ...(post.data.tags || [])].filter(
+        Boolean,
+      ),
+      tag: post.data.tags,
+      author: post.data.author,
+      content: sanitizeHtml(parser.render(post.body ?? ""), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+      }),
+    })),
     customData: `<language>en-us</language>`,
   });
 }
