2d6b39acce84d00921b743cbbf0f238a281a9515
Rahul <deploy.rahul@gmail.com>
2026-05-28 21:21:52 +0530

    refactor: update layouts and pages to use new component architecture

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -6,8 +6,8 @@ import MarkdownIt from "markdown-it";
 const parser = new MarkdownIt();
 
 export async function GET(context: { site: any }) {
-  const posts = (await getCollection("blog"))
-    .filter((post) => !post.data.draft)
+  const items = (await getCollection("blog"))
+    .filter((item) => !item.data.draft)
     .sort((a, b) => {
       const aDate = parse(a.data.date, "dd-MM-yyyy", new Date());
       const bDate = parse(b.data.date, "dd-MM-yyyy", new Date());
@@ -17,20 +17,17 @@ export async function GET(context: { site: any }) {
   return rss({
     trailingSlash: false,
     title: "Ryze Blog",
-    description:
-      "A reader-friendly blog with accessibility, SEO and responsiveness out of the box",
+    description: "A reader-friendly blog with accessibility, SEO and responsiveness out of the box",
     site: context.site,
-    items: posts.map((post) => ({
-      link: `/blog/${post.id}`,
-      pubDate: parse(post.data.date, "dd-MM-yyyy", new Date()),
-      title: post.data.title,
-      description: post.data.description,
-      categories: [post.data.category, ...(post.data.tags || [])].filter(
-        Boolean,
-      ),
-      tag: post.data.tags,
-      author: post.data.author,
-      content: sanitizeHtml(parser.render(post.body ?? ""), {
+    items: items.map((item) => ({
+      link: `/blog/${item.id}`,
+      pubDate: parse(item.data.date, "dd-MM-yyyy", new Date()),
+      title: item.data.title,
+      description: item.data.description,
+      categories: [item.data.category, ...(item.data.tags || [])].filter(Boolean),
+      tag: item.data.tags,
+      author: item.data.author,
+      content: sanitizeHtml(parser.render(item.body ?? ""), {
         allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
       }),
     })),
